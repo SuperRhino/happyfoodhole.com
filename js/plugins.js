26f0a50f6f36e67f2ac6a858d60fdc1a2921db6f
Pasto <rpastorelle@yahoo.com>
2015-09-16 20:31:16 -0400

    Track ga event, too

--- js/plugins.js
+++ js/plugins.js
@@ -22,3 +22,11 @@
 }());
 
 // Place any jQuery/helper plugins in here.
+
+// Click on big button:
+$(function() {
+    $('a.btn-lg').on('click', function(){
+        if (ga) ga('send', 'event', 'buttons', 'click', 'stay tuned');
+        console.log('send', 'event', 'buttons', 'click', 'stay tuned');
+    });
+});
